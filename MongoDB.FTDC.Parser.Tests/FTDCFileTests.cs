d1a85383d2b7a818c164d546cfee368b6df2a68a
Alex Bevilacqua <alex@alexbevi.com>
2020-01-03 11:13:09 -0500

    Forgot to add files in previous commit

--- MongoDB.FTDC.Parser.Tests/FTDCFileTests.cs
+++ MongoDB.FTDC.Parser.Tests/FTDCFileTests.cs
@@ -21,5 +21,15 @@ namespace MongoDB.FTDC.Parser.Tests
             ftdc.Open(@"diagnostic.data\metrics.2020-01-02T11-02-43Z-00000");
             ftdc.Contents.Count.ShouldNotBe(0);
         }
+
+        [TestMethod]
+        public void ParsedFTDCFileShouldDecodeData()
+        {
+            var ftdc = new FTDCFile();
+            ftdc.Open(@"diagnostic.data\metrics.2020-01-02T11-02-43Z-00000");
+            ftdc.Contents.Count.ShouldNotBe(0);
+
+            Should.NotThrow(() => ftdc.Contents[1].DecompressData());
+        }
     }
 }
--- MongoDB.FTDC.Parser/FTDCFile.cs
+++ MongoDB.FTDC.Parser/FTDCFile.cs
@@ -7,7 +7,21 @@ using MongoDB.Bson.Serialization;
 
 namespace MongoDB.FTDC.Parser
 {
+    using CompressionLevel = ZLibNet.CompressionLevel;
+    using CompressionMode = ZLibNet.CompressionMode;
     using Metric = System.Int64;
+
+    public class Chunk
+    {
+        public Metric TMin { get; internal set; }
+        public Metric TMax { get; internal set; }
+
+        public BsonDocument ReferenceDoc { get; internal set; }
+        public int NumKeys { get; internal set; }
+        public int NumDeltas { get; internal set; }
+
+        public byte[] Deltas { get; internal set; }
+    }
 
     public class FTDCFileContents
     {
@@ -15,6 +29,36 @@ namespace MongoDB.FTDC.Parser
         public int type { get; set; }
         public BsonDocument doc { get; set; }
         public BsonBinaryData data { get; set; }
+
+        public BsonDocument DecompressedData { get; internal set; }
+
+        public void DecompressData()
+        {
+            byte[] raw = data.AsByteArray;
+            byte[] compressed = new byte[raw.Length - 4];
+            Array.Copy(raw, 4, compressed, 0, compressed.Length);
+
+            using var inputstr = new MemoryStream(compressed);
+            using (var stream = new ZLibNet.ZLibStream(inputstr, CompressionMode.Decompress))
+            {
+                const int size = 256;
+                byte[] buffer = new byte[size];
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    int count = 0;
+                    do
+                    {
+                        count = stream.Read(buffer, 0, size);
+                        if (count > 0)
+                        {
+                            memory.Write(buffer, 0, count);
+                        }
+                    }
+                    while (count > 0);
+                    DecompressedData = BsonSerializer.Deserialize<BsonDocument>(memory.ToArray());
+                }
+            }
+        }
     }
 
     public class FTDCFile
